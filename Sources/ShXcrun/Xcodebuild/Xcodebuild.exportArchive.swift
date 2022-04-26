09be27adc8a7af4a1cfea117fdcdfa02fcb61a7e
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2022-04-26 00:36:47 -0400

    export archive fix

--- Sources/ShXcrun/Xcodebuild/Xcodebuild.exportArchive.swift
+++ Sources/ShXcrun/Xcodebuild/Xcodebuild.exportArchive.swift
@@ -3,11 +3,10 @@ import Sh
 extension Xcodebuild {
 
   public func exportArchive(_ sink: Sink = .terminal, archivePath: String, exportPath: String? = nil, exportOptionsPlistPath: String) throws {
-    var buffer = "-archivePath \(archivePath) -exportOptionsPlist \(exportOptionsPlistPath)"
+    var buffer = "xcrun xcodebuild -exportArchive -archivePath \(archivePath) -exportOptionsPlist \(exportOptionsPlistPath)"
     if let exportPath = exportPath {
-      buffer.append("-exportPath \(exportPath)")
+      buffer.append(" -exportPath \(exportPath)")
     }
-    buffer.append(" -exportArchive")
-    try sh(sink, serializedCommand(action: buffer))
+    try sh(sink, buffer)
   }
 }
