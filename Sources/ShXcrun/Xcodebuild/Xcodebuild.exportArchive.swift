b1cbcc0d56f9111bea362da417b267bffdf70c38
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2022-05-20 01:13:28 -0400

    export archive needs more options

--- Sources/ShXcrun/Xcodebuild/Xcodebuild.exportArchive.swift
+++ Sources/ShXcrun/Xcodebuild/Xcodebuild.exportArchive.swift
@@ -4,9 +4,19 @@ extension Xcodebuild {
 
   public func exportArchive(_ sink: Sink = .terminal, archivePath: String, exportPath: String? = nil, exportOptionsPlistPath: String) throws {
     var buffer = "xcrun xcodebuild -exportArchive -archivePath \(archivePath) -exportOptionsPlist \(exportOptionsPlistPath)"
+    
     if let exportPath = exportPath {
       buffer.append(" -exportPath \(exportPath)")
     }
+    
+    if allowProvisioningUpdates {
+      buffer.append(" -allowProvisioningUpdates")
+    }
+    
+    if allowProvisioningDeviceRegistration {
+      buffer.append(" -allowProvisioningDeviceRegistration")
+    }
+    
     try sh(sink, buffer)
   }
 }
