2be8a6bc36a34d7811d2d5f09142eaaebb42e17a
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2022-05-20 01:53:32 -0400

    cleanup

--- Sources/ShXcrun/Altool/Altool.uploadApp.swift
+++ Sources/ShXcrun/Altool/Altool.uploadApp.swift
@@ -4,11 +4,15 @@ import Foundation
 extension Altool {
 
   public func uploadApp(_ sink: Sink = .terminal, file: String, platform: Platform, transport: [Transport]? = nil) throws {
+   
+    var buffer = "xcrun altool \(credential.serialized) --upload-app -f \(file) -t \(platform)"
     
-    let cred = credential.serialized
-    let trans = transport.serialized
+    if let transport = transport {
+      buffer.append(transport.serialized)
+    }
+    
     try sh(sink,
-           "xcrun altool \(cred) --upload-app -f \(file) -t \(platform) \(trans)",
+           buffer,
            environment: self.credential.environment)
   }
 }
